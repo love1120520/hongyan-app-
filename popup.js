d934d4a2ea8357a6fa14c47c367005427ab257c2
鴻宴國際新金專業貸款服務 <wangxiaoyix@gmail.com>
2025-08-06 01:57:35 +0800

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,12 @@
+function sendJoin() {
+  alert("👉 撥打電話指令已發出（可連接 Join）");
+}
+
+function playVoice() {
+  alert("🔊 播放語音指令（接 Tasker 語音播放）");
+}
+
+function sendSMS() {
+  alert("💬 已發送簡訊（或透過 Join 觸發）");
+}
+
